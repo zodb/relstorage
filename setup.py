0323435fecbad8a6a0af791050e3f86d80dae60f
Shane Hathaway <shane@hathawaymix.org>
2008-03-12 02:40:11 +0000

    RelStorage 1.0.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ A small patch to ZODB is required.  See the patch files distributed
 with RelStorage.
 """
 
-VERSION = "1.0"
+VERSION = "1.0.1"
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
