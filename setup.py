f82b0a48ac80ebbff67ce53e64983530c0e142ff
Shane Hathaway <shane@hathawaymix.org>
2011-02-04 14:30:11 +0000

    back to dev

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 ##############################################################################
 """A backend for ZODB that stores pickles in a relational database."""
 
-VERSION = "1.4.2"
+VERSION = "1.4.3dev"
 
 # The choices for the Trove Development Status line:
 # Development Status :: 5 - Production/Stable
