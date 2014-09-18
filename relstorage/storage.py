f77e13282731f8bd1e6f9374f33a312c2cbcfaeb
mamico <mauro@biodec.com>
2014-09-18 14:53:32 +0200

    fix zodbconvert incremental

--- relstorage/storage.py
+++ relstorage/storage.py
@@ -1394,17 +1394,17 @@ class RelStorage(
         finally:
             self._lock_release()
 
-    def copyTransactionsFrom(self, other):
+    def copyTransactionsFrom(self, other, next_tid=None):
         # adapted from ZODB.blob.BlobStorageMixin
         begin_time = time.time()
         txnum = 0
         total_size = 0
         log.info("Counting the transactions to copy.")
         num_txns = 0
-        for _ in other.iterator():
+        for _ in other.iterator(start=next_tid):
             num_txns += 1
-        log.info("Copying the transactions.")
-        for trans in other.iterator():
+        log.info("Copying the %s transactions.", num_txns)
+        for trans in other.iterator(start=next_tid):
             txnum += 1
             num_txn_records = 0
 
