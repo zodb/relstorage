eaa411d9879191ae85e56579c21c4a74a5635967
Jason Madden <jamadden@gmail.com>
2016-06-09 10:07:08 -0500

    svb

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 ##############################################################################
 """A backend for ZODB that stores pickles in a relational database."""
 
-VERSION = "1.6.0"
+VERSION = "1.6.1.dev0"
 
 # The choices for the Trove Development Status line:
 # Development Status :: 5 - Production/Stable
