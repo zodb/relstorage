55436c7aaf31849382d159c2fbed141049e0d54a
Jason Madden <jamadden@gmail.com>
2016-06-09 11:01:51 -0500

    Travis CI for MySQL and Postgres.
    Adds .travis.yml and scripts for creating the DBs for those tests.
    Travis is setup to send coverage reports to coveralls.io (once the tests
    pass).
    
    Also adds a tox.ini and .coveragerc for checking coverage locally.
    
    Run the tests with '-m relstorage.tests.alltests', which now behaves
    like the existing '-m relstorage.tests.testmysql' et al.
    
    Fixes an issue running blob tests with '-m'.

--- relstorage/tests/alltests.py
+++ relstorage/tests/alltests.py
@@ -12,12 +12,13 @@
 #
 ##############################################################################
 """Combines the tests of all adapters"""
-
+from __future__ import absolute_import
 import unittest
+import logging
 
-from testpostgresql import test_suite as postgresql_test_suite
-from testmysql import test_suite as mysql_test_suite
-from testoracle import test_suite as oracle_test_suite
+from .testpostgresql import test_suite as postgresql_test_suite
+from .testmysql import test_suite as mysql_test_suite
+from .testoracle import test_suite as oracle_test_suite
 
 def make_suite():
     suite = unittest.TestSuite()
@@ -25,3 +26,10 @@ def make_suite():
     suite.addTest(mysql_test_suite())
     suite.addTest(oracle_test_suite())
     return suite
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    # We get constant errors about failing to lock a blob file,
+    # which really bloats the CI logs, so turn those off.
+    logging.getLogger('zc.lockfile').setLevel(logging.CRITICAL)
+    unittest.main(defaultTest='make_suite')
