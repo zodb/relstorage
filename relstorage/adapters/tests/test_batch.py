e3c056403780cb061d481b320ff105080358d07c
Jason Madden <jamadden@gmail.com>
2016-06-15 17:00:59 -0500

    More batcher tests.

--- relstorage/adapters/tests/test_batch.py
+++ relstorage/adapters/tests/test_batch.py
@@ -306,4 +306,9 @@ class MockCursor:
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(RowBatcherTests))
+    suite.addTest(unittest.makeSuite(PostgreSQLRowBatcherTests))
+    suite.addTest(unittest.makeSuite(OracleRowBatcherTests))
     return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
