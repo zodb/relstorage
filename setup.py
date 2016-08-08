39deffc83e8dd6a810a9ad9f0b0c245f6291ee9a
Mauro Amico <mauro.amico@gmail.com>
2016-08-08 13:19:35 +0200

    fix requirements

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     zip_safe=False,  # otherwise ZConfig can't see component.xml
     install_requires=[
         'perfmetrics',
-        'ZODB3>=3.7.0',
+        'ZODB3>=3.7.0,<3.11.0',
         'zope.interface',
         'zc.lockfile',
     ],
