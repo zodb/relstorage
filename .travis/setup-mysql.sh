bf286a958ed2e6f0d5d30006587cd24cba958344
Mauro Amico <mauro.amico@gmail.com>
2016-08-08 14:00:37 +0200

    Revert "try to fix: MySQL server has gone away"
    This reverts commit aed97bdd1cb1bf856332940788e117b76c0f1d4a.

--- .travis/setup-mysql.sh
+++ .travis/setup-mysql.sh
@@ -1,6 +1,4 @@
 pip install -U -e ".[mysql]"
-echo -e "[server]\nmax_allowed_packet=64M" | sudo tee -a /etc/mysql/conf.d/cope.cnf
-sudo service mysql restart
 mysql -uroot -e "CREATE USER 'relstoragetest'@'localhost' IDENTIFIED BY 'relstoragetest';"
 mysql -uroot -e "CREATE DATABASE relstoragetest;"
 mysql -uroot -e "GRANT ALL ON relstoragetest.* TO 'relstoragetest'@'localhost';"
