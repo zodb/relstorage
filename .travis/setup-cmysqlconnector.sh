ee1fe37b703a2db178dcc6586cddbf6e0d2c47bf
Jason Madden <jamadden@gmail.com>
2017-01-25 10:35:25 -0600

    Try to build the C extension.

--- .travis/setup-cmysqlconnector.sh
+++ .travis/setup-cmysqlconnector.sh
@@ -1,2 +1,5 @@
-pip install https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.5.tar.gz
+wget https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.5.tar.gz
+tar -xf mysql-connector-python-2.1.5.tar.gz
+cd ./mysql-connector-python-2.1.5
+python ./setup.py install --with-mysql-capi=/usr
 `dirname $0`/mysql.sh
