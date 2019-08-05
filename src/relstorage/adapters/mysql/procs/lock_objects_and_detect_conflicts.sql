0763321eba43d0ad2ebf2fe532000be594ee187b
Jason Madden <jamadden@gmail.com>
2019-08-05 13:10:45 -0500

    Try to fix JSON encoding issue.
    I could reproduce it with Python 2 locally, but not Python 3, which only happened on Travis CI.
    
    Still, the fix may be the same.

--- src/relstorage/adapters/mysql/procs/lock_objects_and_detect_conflicts.sql
+++ src/relstorage/adapters/mysql/procs/lock_objects_and_detect_conflicts.sql
@@ -1,11 +1,12 @@
 CREATE PROCEDURE lock_objects_and_detect_conflicts(
-  read_current_oids_tids JSON
+  read_current_oids_tids_text TEXT
 )
   COMMENT '{CHECKSUM}'
 BEGIN
   DECLARE dummy BIGINT;
   DECLARE len INT;
   DECLARE i INT DEFAULT 0;
+  DECLARE read_current_oids_tids JSON;
 
   SELECT COUNT(*)
   FROM (
@@ -25,7 +26,16 @@ BEGIN
   -- We detect the MySQL version when we install the
   -- procedure and choose the appropriate statements.
 
-  IF read_current_oids_tids IS NOT NULL THEN
+  IF read_current_oids_tids_text IS NOT NULL THEN
+    -- We must send the parameter in as TEXT; on Python 2 with
+    -- mysqlclient, trying to send it directly as JSON always fails
+    -- with "Cannot create a JSON value from a string with CHARACTER
+    -- SET 'binary'.", no matter what the character_set_client or
+    -- character_set_connection is, and no matter whether the
+    -- parameter in Python is bytes or unicode. This has also been seen
+    -- with Python 3 on Travis CI, but not locally on the mac, so there's
+    -- some system dependent behaviour involved.
+    SET read_current_oids_tids = CAST(read_current_oids_tids_text AS JSON);
     SET len = JSON_LENGTH(read_current_oids_tids);
     WHILE i < len DO
       INSERT INTO temp_read_current (zoid, tid)
