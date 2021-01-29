dbd199d6d85e3c9d4935a4fe3aa288f91df8e6e2
Jason Madden <jamadden@gmail.com>
2021-01-29 13:32:02 -0600

    Need to specify the database name. [skip ci]

--- .travis/postgres.sh
+++ .travis/postgres.sh
@@ -1,7 +1,7 @@
 RS_PG_UNAME=${RELSTORAGETEST_PG_UNAME:-postgres}
-psql -U $RS_PG_UNAME -c "CREATE USER relstoragetest WITH PASSWORD 'relstoragetest';"
-psql -U $RS_PG_UNAME -c "CREATE DATABASE relstoragetest OWNER relstoragetest;"
-psql -U $RS_PG_UNAME -c "CREATE DATABASE relstoragetest2 OWNER relstoragetest;"
-psql -U $PS_PG_UNAME -c "CREATE DATABASE relstoragetest_hf OWNER relstoragetest;"
-psql -U $RS_PG_UNAME -c "CREATE DATABASE relstoragetest2_hf OWNER relstoragetest;"
-psql -U $RS_PG_UNAME -c "SELECT version();"
+psql -U $RS_PG_UNAME $RELSTORAGETEST_PG_DBNAME -c "CREATE USER relstoragetest WITH PASSWORD 'relstoragetest';"
+psql -U $RS_PG_UNAME $RELSTORAGETEST_PG_DBNAME -c "CREATE DATABASE relstoragetest OWNER relstoragetest;"
+psql -U $RS_PG_UNAME $RELSTORAGETEST_PG_DBNAME -c "CREATE DATABASE relstoragetest2 OWNER relstoragetest;"
+psql -U $RS_PG_UNAME $RELSTORAGETEST_PG_DBNAME -c "CREATE DATABASE relstoragetest_hf OWNER relstoragetest;"
+psql -U $RS_PG_UNAME $RELSTORAGETEST_PG_DBNAME -c "CREATE DATABASE relstoragetest2_hf OWNER relstoragetest;"
+psql -U $RS_PG_UNAME $RELSTORAGETEST_PG_DBNAME -c "SELECT version();"
