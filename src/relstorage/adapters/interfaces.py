zodb/relstorage

commit 08259fa909b63f25e5b07b2f866c7d82104460a3

Finer control over sqlite storage locking, oid allocation and stats. This makes it consistently a few orders of magnitude faster in most benchmarks.
Also expose all the pragmas in the configuration so users can tune it appropriately for their environment. This changes the config schema a bit, so since we were doing that, clean that up a bit.

Documentation still needs updated.

src/relstorage/adapters/interfaces.py
@@ -449,6 +449,21 @@ class IConnectionManager(Interface):
         :return: ``(conn, cursor)``
         """
 
+    def open_for_pack_lock():
+        """
+        Open a connection to be used for the sole purpose of holding
+        the pack lock.
+
+        Use a private connection (lock_conn and lock_cursor) to hold
+        the pack lock. Have the adapter open temporary connections
+        to do the actual work, allowing the adapter to use special
+        transaction modes for packing, and to commit at will without
+        losing the lock.
+
+        If the database doesn't actually use a pack lock,
+        this may return ``(None, None)``.
+        """
+
     def cursor_for_connection(conn):
         """
         If the cursor returned by an open method was discarded

src/relstorage/adapters/mover.py
@@ -25,6 +25,7 @@ from .._compat import OID_TID_MAP_TYPE
 from .._compat import metricmethod_sampled
 from ._util import noop_when_history_free
 from ._util import query_property as _query_property
+from ._util import DatabaseHelpersMixin
 from .._compat import ABC
 from .batch import RowBatcher
 from .interfaces import IObjectMover
@@ -38,7 +39,7 @@ object_state = Schema.object_state
 
 
 @implementer(IObjectMover)
-class AbstractObjectMover(ABC):
+class AbstractObjectMover(DatabaseHelpersMixin, ABC):
 
     def __init__(self, database_driver, options, runner=None,
                  version_detector=None,
@@ -335,15 +336,12 @@ class AbstractObjectMover(ABC):
     ).where(
         # Some databases may be able to benefit from prev_tid <> 0, but it depends
         # on their ability to make use of indexes
-        Schema.temp_store.c.prev_tid != Schema.all_current_object_state.c.tid
-    ).order_by(
-        Schema.temp_store.c.zoid
+        Schema.all_current_object_state.c.tid != Schema.temp_store.c.prev_tid
     ).prepared()
 
     @metricmethod_sampled
     def detect_conflict(self, cursor):
-        stmt = self._detect_conflict_query
-        stmt.execute(cursor)
+        self._detect_conflict_query.execute(cursor)
         # Note that we're not transforming the state into
         # bytes; it doesn't seem to be needed here, even with sqlite3
         # on Python 2 (where it is a buffer).

src/relstorage/adapters/packundo.py
@@ -32,7 +32,7 @@ from ..treemark import TreeMarker
 
 from .schema import Schema
 from .connections import LoadConnection
-from .connections import StoreConnection
+from .connections import PrePackConnection
 from .interfaces import IPackUndo
 from ._util import DatabaseHelpersMixin
 from .sql import it
@@ -634,7 +634,7 @@ class HistoryPreservingPackUndo(PackUndo):
         much faster.
         """
         load_connection = LoadConnection(self.connmanager)
-        store_connection = StoreConnection(self.connmanager)
+        store_connection = PrePackConnection(self.connmanager)
         try:
             # The pre-pack functions are responsible for managing
             # their own commits; when they return, the transaction
@@ -1297,7 +1297,7 @@ class HistoryFreePackUndo(PackUndo):
             return
 
         load_connection = LoadConnection(self.connmanager)
-        store_connection = StoreConnection(self.connmanager)
+        store_connection = PrePackConnection(self.connmanager)
         try:
             try:
                 self._pre_pack_main(load_connection, store_connection,

src/relstorage/storage/tpc/temporary_storage.py
@@ -86,7 +86,6 @@ class TemporaryStorage(object):
         """
         Return the bytes for a previously stored temporary item.
         """
-        __traceback_info__ = dict(self._queue_contents.items())
         startpos, endpos, _ = self._queue_contents[oid_int]
         return self._read_temp_state(startpos, endpos)
 

src/relstorage/storage/tpc/vote.py
@@ -67,7 +67,7 @@ class DatabaseLockedForTid(object):
     @classmethod
     def lock_database_for_given_tid(cls, tid, tid_is_packed,
                                     cursor, adapter, ude):
-        adapter.locker.hold_commit_lock(cursor, ensure_current=True)
+        adapter.locker.hold_commit_lock(cursor, ensure_current=(tid,))
         tid_int = bytes8_to_int64(tid)
         user, desc, ext = ude
         adapter.txncontrol.add_transaction(
@@ -381,7 +381,11 @@ class AbstractVote(AbstractTPCState):
             oid = int64_to_8bytes(oid_int)
             committedSerial = int64_to_8bytes(committed_tid_int)
             oldSerial = int64_to_8bytes(tid_this_txn_saw_int)
-            newpickle = read_temp(oid_int)
+            try:
+                newpickle = read_temp(oid_int)
+            except KeyError:
+                print(self.store_connection.cursor.execute("select * from temp_store").fetchall())
+                raise
 
             # Because we're using the _CachedConflictResolver, we can only loadSerial()
             # one state: the ``oldSerial`` state. Therefore the committedData *must* be
@@ -591,10 +595,5 @@ class _CachedConflictResolver(ConflictResolvingStorage):
 
     def loadSerial(self, oid, serial):
         state, tid = self._old_states_and_tids[bytes8_to_int64(oid)]
-        if bytes8_to_int64(serial) != tid: # pragma: no cover
-            # This should not be possible
-            raise AssertionError(
-                "Load connection got invalid old state. Expected: %s; but got %s." %(
-                    tid, bytes8_to_int64(serial)
-                ))
+        assert bytes8_to_int64(serial) == tid
         return state

src/relstorage/adapters/sqlite/mover.py
@@ -19,9 +19,11 @@ from __future__ import print_function
 
 from zope.interface import implementer
 
+from relstorage._util import consume
 
 from ..interfaces import IObjectMover
 from ..schema import Schema
+from .scriptrunner import Sqlite3ScriptRunner
 from ..mover import AbstractObjectMover
 from ..mover import metricmethod_sampled
 
@@ -30,6 +32,7 @@ from .batch import Sqlite3RowBatcher
 
 @implementer(IObjectMover)
 class Sqlite3ObjectMover(AbstractObjectMover):
+    _create_temp_store = Schema.temp_store.create()
 
     def __init__(self, database_driver, options, runner=None,
                  version_detector=None,
@@ -39,29 +42,36 @@ class Sqlite3ObjectMover(AbstractObjectMover):
             options, runner=runner, version_detector=version_detector,
             batcher_factory=batcher_factory)
 
-    _create_temp_store = Schema.temp_store.create()
-    _clear_temp_store = Schema.temp_store.truncate()
+        # We only ever have one blob chunk so we can simplify that
+        # table.
+        self.__on_store_opened_script = """
+        CREATE TEMPORARY TABLE IF NOT EXISTS temp_blob_chunk (
+                zoid        INTEGER PRIMARY KEY,
+                chunk_num   INTEGER NOT NULL,
+                chunk       BLOB,
+                CHECK(chunk_num = 0)
+        );
+        """ + str(self._create_temp_store) + """;
+        """
 
     @metricmethod_sampled
     def on_store_opened(self, cursor, restart=False):
         """Create the temporary table for storing objects"""
-        if restart:
-            self._clear_temp_store.execute(cursor)
-            cursor.execute('DELETE FROM temp_blob_chunk')
-        else:
-            self._create_temp_store.execute(cursor)
-            # TODO: We don't actually need more than one chunk.
-            stmt = """
-            CREATE TEMPORARY TABLE temp_blob_chunk (
-                zoid        INTEGER NOT NULL,
-                chunk_num   INTEGER NOT NULL,
-                chunk       BLOB,
-                PRIMARY KEY (zoid, chunk_num)
-            )
-            """
-            cursor.execute(stmt)
+        # Don't use cursor.executescript, it commits. Remember our store
+        # connection is usually meant to be in auto-commit mode and shouldn't take
+        # exclusive locks until tpc_vote time. Prior to Python 3.6, if we were
+        # in a transaction this would commit it. but we shouldn't be in a transaction.
+        assert not cursor.connection.in_transaction
+        if not restart:
+            Sqlite3ScriptRunner().run_script(cursor, self.__on_store_opened_script)
+            assert not cursor.connection.in_transaction
+            cursor.connection.register_before_commit_cleanup(self._before_commit)
 
         super(Sqlite3ObjectMover, self).on_store_opened(cursor, restart)
+
+    def _before_commit(self, connection, _rolling_back=None):
+        consume(connection.execute('DELETE FROM temp_store'))
+        consume(connection.execute('DELETE FROM temp_blob_chunk'))
 
     _upload_blob_uses_chunks = False
 
@@ -69,3 +79,16 @@ class Sqlite3ObjectMover(AbstractObjectMover):
     def restore(self, cursor, batcher, oid, tid, data):
         self._generic_restore(batcher, oid, tid, data,
                               command='INSERT OR REPLACE', suffix='')
+
+    def store_temps(self, cursor, state_oid_tid_iter):
+        AbstractObjectMover.store_temps(self, cursor, state_oid_tid_iter)
+        # That should have started a transaction, effectively moving us
+        # from READ COMMITTED mode into REPEATABLE READ. We don't want that (but we do
+        # want the write to be transactional and fast) so we now COMMIT and go back to
+        # floating.
+        cursor.connection.commit(run_cleanups=False)
+
+    def replace_temps(self, cursor, state_oid_tid_iter):
+        # This is called when we're already holding exclusive locks
+        # on the DB and must not release them.
+        AbstractObjectMover.store_temps(self, cursor, state_oid_tid_iter)

src/relstorage/_compat.py
@@ -37,6 +37,7 @@ __all__ = [
     # Constants
     'PY3',
     'PY2',
+    'PY36',
     'PYPY',
     'WIN',
     'MAC',
@@ -72,6 +73,7 @@ __all__ = [
 ]
 
 PY3 = sys.version_info[0] == 3
+PY36 = sys.version_info[:2] >= (3, 6)
 PY2 = not PY3
 PYPY = platform.python_implementation() == 'PyPy'
 WIN = sys.platform.startswith('win')

src/relstorage/adapters/_util.py
@@ -12,6 +12,8 @@
 #
 ##############################################################################
 "Internal helper utilities."
+from __future__ import print_function
+from __future__ import absolute_import
 
 from collections import namedtuple
 from functools import partial
@@ -186,7 +188,11 @@ class DatabaseHelpersMixin(object):
         """
         Return the rows formatted in a way for easy human consumption.
         """
-        # This could be tabular, but its easiest just to use pprint
-        import pprint
-        rows = list(self._rows_as_dicts(cursor))
-        return pprint.pformat(rows)
+        from .._compat import NStringIO
+        out = NStringIO()
+        names = [d.name for d in self._column_descriptions(cursor)]
+        kwargs = {'sep': '\t', 'file': out}
+        print(*names, **kwargs)
+        for row in cursor:
+            print(*row, **kwargs)
+        return out.getvalue()

commit 26e5bea43b5931acae8475eb4807e11e0f882616

Fix deleteObject in history-preserving mode to conform to ZODB interface
ZODB specifies deleteObject to create new revision that indicates object
removal:

    def deleteObject(oid, serial, transaction):
        """Mark an object as deleted

        This method marks an object as deleted VIA A NEW OBJECT
        REVISION.  Subsequent attempts to load current data for the
        object will fail with a POSKeyError, but loads for
        non-current data will succeed if there are previous
        non-delete records.  The object will be removed from the
        storage when all not-delete records are removed.

https://github.com/zopefoundation/ZODB/blob/bc13ca74/src/ZODB/interfaces.py#L1292-L1307
(emphasis mine)

However currently for history-preserving mode, as explained in
https://github.com/zopefoundation/ZODB/issues/318#issuecomment-657683419,
RelStorage purges latest object revision instead of creating new one with
whiteout indication. This goes against deleteObject specification and, as
demonstrated by attached test program, against particular FileStorage
behaviour.

-> Fix it.

P.S. I'm complete RelStorage newbie and looked only briefly. It could be that
my patch is e.g. incomplete, or not optimal. However it demonstrates a real
problem, and it fixes both adjusted testcase and failure of attached tdelete.py

P.P.S. Tested only with SQLite backend.

    ---- 8< ---- (tdelete.py)
    #!/usr/bin/env python
    """tdelete.py demonstrates that deleteObject should create new whiteout
    record, and that older data records should be still accessible.

    e.g. with FileStorage:

       $ ./tdelete.py file://1.db
       @03e40964a0766f33 (= 280359404597309235) obj<0000000000000001>  ->  int(0)
       @03e40964a0790944 (= 280359404597479748) obj<0000000000000001>  ->  int(1)

       --------

       @03e40964a0766f33  obj<0000000000000001>  ->  int(0)    # must be int(0)
       @03e40964a0790944  obj<0000000000000001>  ->  int(1)    # must be int(1)

    However it currently fails with RelStorage, because deleteObject does not
    create new whiteout revision and instead purges already committed data:

        $ rm x/*; ./tdelete.py sqlite://?data_dir=`pwd`/x
        @03e40972d5408022 (= 280359465612509218) obj<0000000000000001>  ->  int(0)
        @03e40972d541ddee (= 280359465612598766) obj<0000000000000001>  ->  int(1)

        --------

        @03e40972d5408022  obj<0000000000000001>  ->  int(0)    # must be int(0)
        Traceback (most recent call last):
          File "./tdelete.py", line 84, in <module>
            main()
          File "./tdelete.py", line 80, in main
            dumpObjAt(at1, "must be int(1)")
          File "./tdelete.py", line 75, in dumpObjAt
            obj = conn.get(oid)
          File "/home/kirr/src/wendelin/z/ZODB/src/ZODB/Connection.py", line 238, in get
            obj = self._reader.getGhost(p)
          File "/home/kirr/src/wendelin/z/ZODB/src/ZODB/serialize.py", line 598, in getGhost
            unpickler = self._get_unpickler(pickle)
          File "/home/kirr/src/wendelin/z/ZODB/src/ZODB/serialize.py", line 478, in _get_unpickler
            file = BytesIO(pickle)
        TypeError: StringIO() argument 1 must be string or buffer, not None
    """

    from __future__ import print_function

    import zodburi
    from persistent import Persistent
    from ZODB.DB import DB
    from ZODB.Connection import TransactionMetaData
    from ZODB.utils import u64
    import transaction
    import sys

    class PInt(Persistent):
        def __init__(self, i):
            self.i = i
        def __str__(self):
            return "int(%d)" % self.i

    def h(tid):
        return tid.encode('hex')

    def dump(obj):
        print("@%s (= %d) obj<%s>  ->  %s" % (h(obj._p_serial), u64(obj._p_serial), h(obj._p_oid), obj))

    def main():
        zurl = sys.argv[1]
        zstoropen, dbkw = zodburi.resolve_uri(zurl)

        stor = zstoropen()
        db = DB(stor, **dbkw)

        conn = db.open()
        root = conn.root()

        root['X'] = obj = PInt(0)
        transaction.commit()
        dump(obj)
        at0 = obj._p_serial
        oid = obj._p_oid

        obj.i += 1
        transaction.commit()
        dump(obj)
        at1 = obj._p_serial

        txn_meta = TransactionMetaData()
        stor.tpc_begin(txn_meta)
        stor.deleteObject(oid, at1, txn_meta)
        stor.tpc_vote(txn_meta)
        stor.tpc_finish(txn_meta)

        print('\n--------\n')

        def dumpObjAt(at, comment):
            conn = db.open(at=at)
            obj = conn.get(oid)
            print("@%s  obj<%s>  ->  %s\t# %s" % (h(at), h(oid), obj, comment))
            conn.close()

        dumpObjAt(at0, "must be int(0)")
        dumpObjAt(at1, "must be int(1)")

    if __name__ == '__main__':
        main()

P.P.P.S. SQLite URI resolver is currently broken after 08259fa909b6 (Finer

src/relstorage/adapters/interfaces.py
@@ -1020,7 +1020,7 @@ class IPackUndo(Interface):
         accept two parameters, oid and tid (64 bit integers).
         """
 
-    def deleteObject(cursor, oid_int, tid_int):
+    def deleteObject(shared_state, oid_int, tid_int):
         """
         Delete the revision of *oid_int* in transaction *tid_int*.
 
@@ -1042,9 +1042,10 @@ class IPackUndo(Interface):
         is at *tid_int*), leading all access to *oid_int* in the
         future to throw ``POSKeyError``.
 
-        In history preserving databases, this means to set the state for the object
-        at the transaction to NULL, signifying that it's been deleted. A subsequent
-        pack operation is required to actually remove these deleted items.
+        In history preserving databases, this means to create new object
+        revision with state=NULL , signifying that the object has been deleted.
+        A subsequent pack operation is required to actually remove these
+        deleted items.
         """
 
 class IPoller(Interface):

src/relstorage/adapters/mover.py
@@ -300,7 +300,7 @@ class AbstractObjectMover(DatabaseHelpersMixin, ABC):
         query.executemany(
             cursor,
             (
-                (oid_int, tid_int, do_md5(data), Binary(data))
+                (oid_int, tid_int, do_md5(data), Binary(data) if data is not None else None)
                 for (data, oid_int, tid_int)
                 in state_oid_tid_iter
             )

src/relstorage/adapters/packundo.py
@@ -309,10 +309,12 @@ class PackUndo(DatabaseHelpersMixin):
                     INNER JOIN object_ref USING (zoid)
                     WHERE keep = %(TRUE)s
                     AND NOT EXISTS (
-                        SELECT 1
-                        FROM object_state
-                        WHERE object_state.zoid = to_zoid
-                        AND object_state.state IS NOT NULL
+                        SELECT state FROM (
+                            SELECT state, MAX(tid)
+                            FROM object_state
+                            WHERE object_state.zoid = to_zoid
+                        )
+                        WHERE state IS NOT NULL
                     )
                     """
                     self.runner.run_script_stmt(ss_load_cursor, stmt)
@@ -343,20 +345,6 @@ class PackUndo(DatabaseHelpersMixin):
     # pack. (We don't do anything with current_object; a state of NULL
     # represents a deleted object; it shouldn't be reachable anyway
     # and will be packed away next time we pack (without GC))
-
-    # We shouldn't *have* to verify the oldserial in the delete statement,
-    # because our only consumer is zc.zodbdgc which only calls us for
-    # unreachable objects, so they shouldn't be modified and get a new
-    # TID. But it's safer to do so.
-    _script_delete_object = None
-
-    def deleteObject(self, cursor, oid, oldserial):
-        params = {'oid': u64(oid), 'tid': u64(oldserial)}
-        self.runner.run_script_stmt(
-            cursor,
-            self._script_delete_object,
-            params)
-        return cursor.rowcount
 
     def on_filling_object_refs_added(self, oids=None, tids=None):
         """Test injection point for packing."""
@@ -475,14 +463,12 @@ class HistoryPreservingPackUndo(PackUndo):
         ).limit(delete_empty_transactions_batch_size))
     )
 
-    _script_delete_object = """
-    UPDATE object_state
-    SET state = NULL,
-        state_size = 0,
-        md5 = ''
-    WHERE zoid = %(oid)s
-    AND    tid = %(tid)s
-    """
+    def deleteObject(self, shared_state, oid, oldserial):
+        oid_int = u64(oid)
+        oldserial_int = u64(oldserial)
+        shared_state.temp_storage.store_temp(oid_int, None, oldserial_int)
+        # can't count how many objects are deleted in history-preserving mode
+        return None
 
     _is_packed_tx_query = Schema.transaction.select(
         1
@@ -1186,6 +1172,14 @@ class HistoryFreePackUndo(PackUndo):
     WHERE zoid = %(oid)s
     and tid = %(tid)s
     """
+    def deleteObject(self, shared_state, oid, oldserial):
+        cursor = shared_state.store_connection.cursor
+        params = {'oid': u64(oid), 'tid': u64(oldserial)}
+        self.runner.run_script_stmt(
+            cursor,
+            self._script_delete_object,
+            params)
+        return cursor.rowcount
 
     def on_fill_object_ref_batch(self, oid_batch, num_refs_found):
         """Hook for testing."""

src/relstorage/storage/tpc/begin.py
@@ -177,7 +177,7 @@ class AbstractBegin(AbstractTPCStateDatabaseAvailable):
 
     def deleteObject(self, oid, oldserial, transaction):
         """
-        This method operates directly against the ``object_state`` table;
+        In history-free mode this method operates directly against the ``object_state`` table;
         as such, it immediately takes out locks on that table.
 
         This method is only expected to be called when performing
@@ -216,12 +216,11 @@ class AbstractBegin(AbstractTPCStateDatabaseAvailable):
 
         # We delegate the actual operation to the adapter's packundo,
         # just like native pack
-        cursor = self.shared_state.store_connection.cursor
         # When this is done, we get a tpc_vote,
         # and a tpc_finish.
         # The interface doesn't specify a return value, so for testing
         # we return the count of rows deleted (should be 1 if successful)
-        deleted = self.shared_state.adapter.packundo.deleteObject(cursor, oid, oldserial)
+        deleted = self.shared_state.adapter.packundo.deleteObject(self.shared_state, oid, oldserial)
         self._invalidated_oids(oid)
         return deleted
 

src/relstorage/storage/tpc/temporary_storage.py
@@ -57,8 +57,11 @@ class TPCTemporaryStorage(object):
         queue = self._queue
         queue.seek(0, 2)  # seek to end
         startpos = queue.tell()
-        queue.write(state)
-        endpos = queue.tell()
+        if state is not None:
+            queue.write(state)
+            endpos = queue.tell()
+        else:
+            endpos = None
         self._queue_contents[oid_int] = (startpos, endpos, prev_tid_int)
 
     def __len__(self):
@@ -88,6 +91,8 @@ class TPCTemporaryStorage(object):
         Return the bytes for a previously stored temporary item.
         """
         startpos, endpos, _ = self._queue_contents[oid_int]
+        if endpos is None:
+            return None
         return self._read_temp_state(startpos, endpos)
 
     def __iter__(self):
@@ -96,7 +101,10 @@ class TPCTemporaryStorage(object):
     def iter_for_oids(self, oids):
         read_temp_state = self._read_temp_state
         for startpos, endpos, oid_int, prev_tid_int in self.items(oids):
-            state = read_temp_state(startpos, endpos)
+            if endpos is None:
+                state = None
+            else:
+                state = read_temp_state(startpos, endpos)
             yield state, oid_int, prev_tid_int
 
     def items(self, oids=None):

src/relstorage/storage/tpc/vote.py
@@ -630,29 +630,11 @@ class HistoryPreservingDeleteOnly(HistoryPreserving):
     __slots__ = ()
 
     def _vote(self, storage):
-        if self.shared_state.temp_storage and self.shared_state.temp_storage.stored_oids:
-            raise StorageTransactionError("Cannot store and delete at the same time.")
-        # We only get here if we've deleted objects, meaning we hold their row locks.
-        # We only delete objects once we hold the commit lock.
-        assert self.committing_tid_lock
-        # Holding the commit lock put an entry in the transaction table,
-        # but we don't want to bump the TID or store that data.
-        self.shared_state.adapter.txncontrol.delete_transaction(
-            self.shared_state.store_connection.cursor,
-            self.committing_tid_lock.tid_int
-        )
-        self.lock_and_vote_times[0] = time.time()
-        return ()
-
-    def _lock_and_move(self, vote_only=False):
-        # We don't do the final commit,
-        # we just prepare.
-        self._enter_critical_phase_until_transaction_end()
-        self.shared_state.prepared_txn = self.shared_state.adapter.txncontrol.commit_phase1(
-            self.shared_state.store_connection,
-            self.committing_tid_lock.tid_int
-        )
-        return False
+        if self.shared_state.temp_storage:
+            for (state, _, _) in self.shared_state.temp_storage:
+                if state is not None:
+                    raise StorageTransactionError("Cannot store and delete at the same time.")
+        return super(HistoryPreservingDeleteOnly, self)._vote(storage)
 
 
 class _CachedConflictResolver(ConflictResolvingStorage):

src/relstorage/tests/hptestbase.py
@@ -400,15 +400,11 @@ class HistoryPreservingRelStorageTests(GenericRelStorageTests,
         history = storage.history(obj_oid, size=100)
         self.assertEqual(6, len(history))
         latest_tid = history[0]['tid']
-        # We can delete the latest TID for the OID, and the whole
+        # We can record deletion for the OID, and the whole
         # object goes away on a pack.
         t = TransactionMetaData()
         storage.tpc_begin(t)
-        count = storage.deleteObject(obj_oid, latest_tid, t)
-        self.assertEqual(count, 1)
-        # Doing it again will do nothing because it's already
-        # gone.
-        count = storage.deleteObject(obj_oid, latest_tid, t)
+        storage.deleteObject(obj_oid, latest_tid, t)
         invalidations = storage.tpc_vote(t)
         storage.tpc_finish(t)
 
@@ -424,6 +420,8 @@ class HistoryPreservingRelStorageTests(GenericRelStorageTests,
             storage.load(obj_oid)
 
         # But we can load a state before then.
+        state = storage.loadSerial(obj_oid, history[0]['tid'])
+        self.assertEqual(len(state), history[0]['size'])
         state = storage.loadSerial(obj_oid, history[1]['tid'])
         self.assertEqual(len(state), history[1]['size'])
 