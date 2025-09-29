5b3f869e14d98d2d0f1689c5a1cca544343151b4
Jason Madden <jamadden@gmail.com>
2025-09-29 11:24:02 -0500

    pg8000 fix for Windows/Postgresql 13.6 not seen elsewhere.

--- src/relstorage/adapters/postgresql/drivers/pg8000.py
+++ src/relstorage/adapters/postgresql/drivers/pg8000.py
@@ -15,23 +15,18 @@
 """
 pg8000 IDBDriver implementations.
 """
-
-from __future__ import absolute_import
-from __future__ import print_function
-
 from collections import deque
 
 from zope.interface import implementer
 
 from relstorage._compat import number_types
 
+from ...._compat import WIN
 from ...interfaces import IDBDriver
 from ...sql import Compiler
-
 from . import AbstractPostgreSQLDriver
 from . import PostgreSQLDialect
 from ._lobject import LobConnectionMixin
-
 
 __all__ = [
     'PG8000Driver',
@@ -249,3 +244,17 @@ class PG8000Driver(AbstractPostgreSQLDriver):
 
     def _get_exception_pgcode(self, exc):
         return exc.args[0]['C']
+
+    if WIN:
+        def exception_is_deadlock(self, exc):
+            is_deadlock = super().exception_is_deadlock(exc)
+            if is_deadlock:
+                return is_deadlock
+            # pg8000 raises pg8000.dbapi.ProgrammingError, which is a DatabaseError.
+            # We've seen both
+            #    40P01 - deadlock_detected (Transaction Rollback)
+            #    55P03 - lock_not_available (Object Not In Prerequisite State)
+            # ERRCODE_DEADLOCK is the first one, checked by super.
+            if isinstance(exc, self.driver_module.DatabaseError):
+                return self._get_exception_pgcode(exc) == '55P03'
+            return None
